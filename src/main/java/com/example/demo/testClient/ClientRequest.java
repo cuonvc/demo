05dabcbf902ef6610b0defec93263c207858d386
Cuong Nguyen <cuong.fithau@gmail.com>
2022-09-06 09:31:49 +0700

    Test (client) random login and create post with jwt

--- src/main/java/com/example/demo/testClient/ClientRequest.java
+++ src/main/java/com/example/demo/testClient/ClientRequest.java
@@ -1,5 +1,7 @@
 package com.example.demo.testClient;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Random;
 
 public class ClientRequest {
 
@@ -40,14 +43,75 @@ public class ClientRequest {
 //        }
 
         //post data
+
+        int number = 20;  //size of table users (start with 0)
+        Random random = new Random();
+        int numRand = random.nextInt(number);  //user id
+        String usename = "";  //get from user id
+        String password = ""; //get from user id
+        loginAccount(usename, password);
+
+    }
+
+    private static void loginAccount(String username, String password) {
         try {
-            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+            URL url = new URL("https://nvc-rest-blog.herokuapp.com/api/v1/auth/login");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setDoOutput(true);
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/json");
 
-            String input = "{\"title\":\"Test title1\",\"body\":\"Test body2\",\"userId\":1}";
+            JSONObject newJson = new JSONObject();
+            newJson.put("usernameOrEmail", username);
+            newJson.put("password", password);
+
+            String stringJson = newJson.toString();
+
+//            String input = "{\"usernameOrEmail\":\"cuongadminnvc\",\"password\":\"cuongnvc\"}";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(stringJson.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = bufferedReader.readLine()) != null) {
+                System.out.println(output);
+                JSONObject jsonObject = new JSONObject(output);
+//                System.out.println(jsonObject.get("accessToken").toString());
+                createPost(jsonObject.get("accessToken").toString());
+            }
+
+            conn.disconnect();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static void createPost(String token) {
+        try {
+            URL url = new URL("https://nvc-rest-blog.herokuapp.com/api/v1/category/4/post");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            String accessToken = "Bearer " + token;
+            conn.setRequestProperty("Authorization", accessToken);
+
+
+            String input = "{\"title\":\"Test title1\",\"description\":\"Test description\",\"content\":\"test content\"}";
 
             OutputStream os = conn.getOutputStream();
             os.write(input.getBytes());
