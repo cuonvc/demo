d29a17447f0d2f978daf67cea6e4924bf2ddc291
sonht_tts <sonht_tts@hyperlogy.com>
2022-09-12 17:09:50 +0700

    Criteria example

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.demo.repository.RepoTestHQL;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.repository.TeacherRepository;
 import com.example.demo.service.StudentService;
+import org.apache.log4j.Logger;
 import org.hibernate.Filter;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import javax.persistence.metamodel.EntityType;
 import javax.persistence.metamodel.Metamodel;
+import java.time.LocalDate;
 import java.util.*;
 
 @Service
@@ -42,6 +42,8 @@ public class StudentServiceImpl implements StudentService {
 //        this.teacherRepository = teacherRepository;
 //        this.entityManager = entityManager;
 //    }
+
+    static Logger logger = Logger.getLogger(StudentService.class.getName());
 
     @Override
     @Transactional
@@ -88,33 +90,79 @@ public class StudentServiceImpl implements StudentService {
     @Transactional
     public Student createStudent(Student student) {
 
-//        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-//        CriteriaQuery criteriaQuery = criteriaBuilder.createQuery(Student.class);
-//        Root<Student> studentRoot = criteriaQuery.from(Student.class);
-//        EntityType<Student> Student_ = studentRoot.getModel();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Student> criteriaQuery = criteriaBuilder.createQuery(Student.class);
+        Root<Student> studentRoot = criteriaQuery.from(Student.class);
+        Join<Student,Subject> studentSubjectJoin = studentRoot.join(Student_.SUBJECTS, JoinType.LEFT);
+//        studentRoot.fetch(Student_.subjects);
+//        studentRoot.fetch(Student_.school);
+
+        System.out.println(studentSubjectJoin.get("subject_name"));
+
+//        distict vs group by
+        //List<Feticate>
+
+//        List<Predicate>
+//        List<Expression> expressions
+//        List<Selection> selection = new ArrayList<>();
+
+        entityManager.createQuery(criteriaQuery)
+                .getResultStream()
+                .forEach(s -> System.out.println(s.getSubjects()));
+
+        return null;
+
+
+
 
         //or
-        Metamodel metamodel = entityManager.getMetamodel();
-        EntityType<Student> Student_ = metamodel.entity(Student.class);
+//        Metamodel metamodel = entityManager.getMetamodel();
+//        EntityType<Student> Student_ = metamodel.entity(Student.class);
 
         //...
 
-        return null;
+//        Student std = studentRepository.save(student);
+//        std.setCreatedDate(LocalDate.now());
+//
+//        return std;
     }
 
     @Override
     public Student getStudent(String name) {
 //        Student student = studentRepository.findStudentByName(name).orElseThrow(RuntimeException::new);
 
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Student> criteriaQuery = criteriaBuilder.createQuery(Student.class);
-        Root<Student> studentRoot = criteriaQuery.from(Student.class);
+//        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<Student> criteriaQuery = criteriaBuilder.createQuery(Student.class);
+//        Root<Student> studentRoot = criteriaQuery.from(Student.class);
+//
+//        criteriaQuery.select(studentRoot).where(studentRoot.get(Student_.fullName).in(name));
+//        TypedQuery<Student> query = entityManager.createQuery(criteriaQuery);
+//
+//        Student student = query.getSingleResult();
 
-        criteriaQuery.select(studentRoot).where(studentRoot.get(Student_.fullName).in(name));
-        TypedQuery<Student> query = entityManager.createQuery(criteriaQuery);
 
-        Student student = query.getSingleResult();
-        return student;
+
+        try {
+            CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+            CriteriaQuery<Student> criteriaQuery = criteriaBuilder.createQuery(Student.class);
+            Root<Student> studentRoot = criteriaQuery.from(Student.class);
+
+            criteriaQuery.select(studentRoot).where(studentRoot.get(Student_.fullName).in(name));
+            TypedQuery<Student> query = entityManager.createQuery(criteriaQuery);
+
+            Student student = query.getSingleResult();
+            logger.info("Get student....");
+            return student;
+        } catch (RuntimeException exception) {
+            logger.error("Student not found !");
+            return null;
+        }
+
+//        logger.info("test logging");
+//        logger.info(student.getFullName());
+//        logger.warn("test Warrning");
+
+//        return student;
     }
 
     @Override
@@ -164,5 +212,11 @@ public class StudentServiceImpl implements StudentService {
         List<Student> allStudents = query.getResultList();
 
         return allStudents;
+    }
+
+    @Override
+    public void deleteStudent(Integer id) {
+        Student student = studentRepository.findById(id).orElseThrow(RuntimeException::new);
+        studentRepository.delete(student);
     }
 }
