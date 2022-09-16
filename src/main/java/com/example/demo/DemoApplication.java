d6ac3b66a84d2d210a36986031b11c630713ba36
sonht_tts <sonht_tts@hyperlogy.com>
2022-09-16 17:34:18 +0700

    Test logging Kafka

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import reactor.core.publisher.Flux;
+
+import java.util.Random;
+import java.util.function.Function;
 
 @SpringBootApplication
 @Slf4j
@@ -20,10 +27,33 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	private StreamBridge streamBridge;
+
 	@Override
 	public void run(String... args) throws Exception, RuntimeException {
 		//logging
-		PropertyConfigurator.configure("C:/Users/Thi Cuong/IdeaProjects/demo/src/main/resources/log4j.properties");
-		log.info("test logging");
+//		PropertyConfigurator.configure("C:/Users/Thi Cuong/IdeaProjects/demo/src/main/resources/log4j.properties");
+//		log.info("test logging");
+
+		//test logging kafka
+		reduceMachineProductMap();
+		int number;
+		while (true) {
+			Random random = new Random();
+			number = random.nextInt(10);
+			streamBridge.send("initialTopic", number);
+		}
+	}
+
+	@Bean
+	public Function<Integer, String> reduceMachineProductMap() {
+		log.info("Received Order and try to reduce product map");
+		return (num) -> {
+//			Random random = new Random();
+//			num = random.nextInt(10);
+			log.info("Number: " + num);
+			return "Number: " + num;
+		};
 	}
 }
